d17e62abc3917d8c63b28190adbbd3b4f1b03961
UrielSpiridione <urielspiridione135@gmail.com>
2022-06-13 21:28:08 -0300

    Moved navbar to its own folder

--- Front-End-Angular/src/app/app.module.ts
+++ Front-End-Angular/src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ExperienceComponent } from './components/experience/experience.componen
 import { FooterComponent } from './components/footer/footer.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { SkillsComponent } from './components/skills/skills.component';
-import { NavbarComponent } from './components/header/navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { GreetingComponent } from './components/header/greeting/greeting.component';
 
 @NgModule({
